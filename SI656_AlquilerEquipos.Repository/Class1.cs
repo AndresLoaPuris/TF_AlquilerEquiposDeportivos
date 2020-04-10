4bba5024ed5c32315467ef9da13b5d42d362c0cf
MarcoDelaCruzPrado <antonio.123147@hotmail.com>
2020-04-10 17:04:21 -0500

    configuracion de capsa

---
+++ SI656_AlquilerEquipos.Repository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI656_AlquilerEquipos.Repository
+{
+    public class Class1
+    {
+    }
+}
