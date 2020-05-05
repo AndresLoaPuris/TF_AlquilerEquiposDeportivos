2275b44922fefeb7bd3cbe61bf3e556fce8f493e
AndresLoaPuris <49095669+AndresLoaPuris@users.noreply.github.com>
2020-05-05 08:01:31 -0500

    TODO : Maters Details - Alquiler

--- SI656_Alquiler_Equipos/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SI656_Alquiler_Equipos.MvcApplication" Language="C#" %>
---
+++ WebApplication_AlquilerEquipos/Controllers/EquiposController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_AlquilerEquipos.Models;
+
+namespace WebApplication_AlquilerEquipos.Controllers
+{
+    [Authorize]
+    public class EquiposController : Controller
+    {
+        private AlquilerEquiposEntities db = new AlquilerEquiposEntities();
+
+        // GET: Equipos
+        public ActionResult Index()
+        {
+            var equipo = db.Equipo.Include(e => e.Estado);
+            return View(equipo.ToList());
+        }
+
+        // GET: Equipos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipos/Create
+        public ActionResult Create()
+        {
+            ViewBag.Estado_Id = new SelectList(db.Estado, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Equipos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Codigo,Nombre,Descripcion,Costo,Estado_Id")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipo.Add(equipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Estado_Id = new SelectList(db.Estado, "Id", "Nombre", equipo.Estado_Id);
+            return View(equipo);
+        }
+
+        // GET: Equipos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Estado_Id = new SelectList(db.Estado, "Id", "Nombre", equipo.Estado_Id);
+            return View(equipo);
+        }
+
+        // POST: Equipos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Codigo,Nombre,Descripcion,Costo,Estado_Id")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Estado_Id = new SelectList(db.Estado, "Id", "Nombre", equipo.Estado_Id);
+            return View(equipo);
+        }
+
+        // GET: Equipos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: Equipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            db.Equipo.Remove(equipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
